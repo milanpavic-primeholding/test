cbe00c098286baa22ae760323fb3b18d05745576
bitmilanpavic <milanpavice@gmail.com>
2020-07-01 09:34:37 +0200

    init commit

---
+++ app-files-initview-hotspot-info-and-link/data.js
@@ -0,0 +1,90 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-8hw0clw-360-panorama-miami",
+      "name": "8hw0clw-360-panorama-miami",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.454891164464895,
+        "pitch": -0.17014206112622787,
+        "fov": 1.3046490827601978
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.16818350714339125,
+          "pitch": -0.001494782546421547,
+          "rotation": 0,
+          "target": "1-k402fcz-a-laptop-on-a-table"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4812553165510458,
+          "pitch": 0.13014359368640882,
+          "title": "Bridge",
+          "text": "Bridge information"
+        }
+      ]
+    },
+    {
+      "id": "1-k402fcz-a-laptop-on-a-table",
+      "name": "k402fcz-a-laptop-on-a-table",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.035390176707748466,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "0-8hw0clw-360-panorama-miami"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ app-files-multiple-panoramas/data.js
@@ -0,0 +1,69 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-8hw0clw-360-panorama-miami",
+      "name": "8hw0clw-360-panorama-miami",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-k402fcz-a-laptop-on-a-table",
+      "name": "k402fcz-a-laptop-on-a-table",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
